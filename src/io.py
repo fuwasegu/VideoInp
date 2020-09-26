a700738c908dc75cca087a4b29ca8dac56eedc17
lunain84 <takeshita.hirosugu.17@shizuoka.ac.jp>
2020-09-26 22:58:21 +0900

    frames2movie()に、フレームレートを指定できる機能を追加

--- src/io.py
+++ src/io.py
@@ -37,12 +37,13 @@ def movie2frames(video_path, dir_path, basename, ext='jpg'):
         else:
             return
 
-def frames2movie(video_name, dir_path, basename, frame_ext='jpg', video_path='./'):
+def frames2movie(video_name, dir_path, basename, fps=30,frame_ext='jpg', video_path='./'):
     """ディレクトリ内の全てのフレームを使って動画を生成する。
     【引数】
         video_name: 生成する動画のファイル名。拡張子付き。
         dir_path: 動画生成に使用するフレームのディレクトリパス
         basename: 動画生成に使用するフレームのファイル名の共通部分
+        fps: 生成される動画のフレームレート。デフォルトは30。
         frame_ext: 動画生成に使用するフレームの拡張子。デフォルトはjpg。
         video_path: 動画を保存するディレクトリのパス。デフォルトはカレントディレクトリ。
 
@@ -61,7 +62,7 @@ def frames2movie(video_name, dir_path, basename, frame_ext='jpg', video_path='./
         size = (width, height)
         img_array.append(img)
 
-    out = cv2.VideoWriter(video_path+video_name, 0x7634706d, 30, size)
+    out = cv2.VideoWriter(video_path+video_name, 0x7634706d, fps, size)
 
     for i in range(len(img_array)):
         out.write(img_array[i])
